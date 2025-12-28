d1001e4a96027c4119581097d9a3c896b0c06260
Haim Lavi <HaimL1@verifone.com>
2025-12-28 12:49:34 +0200

    3 2

--- Networks3/ba.py
+++ Networks3/ba.py
@@ -1,25 +1,38 @@
+import math
 from matplotlib import pyplot as plt
 import numpy as np
 
 class Node:
     def __init__(self):
-        self.connections: list[Node] = []
+        self.neighbors: list[Node] = []
 
-    def add_connection(self, node: 'Node'):
-        self.connections.append(node)
+    def add_neighbor(self, neighbor: 'Node'):
+        self.neighbors.append(neighbor)
 
 
-def main():
-    counter: int = 0
-
-    kernel_size: int = 4
-
+def run_ba_model(size: int, kernel_size: int):
     list_nodes: list[Node] = create_kernel(kernel_size)
 
-    for step in range(1, 8520):
+    for step in range(1, size):
         new_node: Node = Node()
 
         list_indices: list[int] = [index for index in range(len(list_nodes))]
+
+        max_k: int = 0
+        min_k: int = 0
+
+        for i in range(len(list_nodes)):
+            node: Node = list_nodes[i]
+
+            k_i: int = len(node.neighbors)
+
+            max_k = max(max_k, k_i)
+            min_k = k_i if min_k == 0 else min(min_k, k_i)
+
+        ratio: float = max_k / min_k if min_k > 0 else 0.0
+        square_root_size: float = math.sqrt(len(list_nodes))
+    
+        print(f"size={len(list_nodes)}, ratio={ratio}, sqrt_size={square_root_size}")
 
         probabilities = prepare_probabilities(list_nodes, kernel_size, step)
 
@@ -28,25 +41,37 @@ def main():
         for index in selected_indices:
             node: Node = list_nodes[index]
 
-            node.add_connection(new_node)
-            new_node.add_connection(node)
+            node.add_neighbor(new_node)
+            new_node.add_neighbor(node)
 
         list_nodes.append(new_node)
 
-        list_degrees: list[str] = [str(len(node.connections)) for node in list_nodes]
-
-        print(f"Step: {step}, nodes: {len(list_nodes)}")
-
-    
+        if len(list_nodes) == 54:
+            _ = 0    
 
     node_indices: list[int] = [0] * len(list_nodes)
     ks: list[int] = [0] * len(list_nodes)
 
+    max_k: int = 0
+    min_k: int = 0
+
     for i in range(len(list_nodes)):
         node: Node = list_nodes[i]
 
+        k_i: int = len(node.neighbors)
+
+        max_k = max(max_k, k_i)
+        min_k = k_i if min_k == 0 else min(min_k, k_i)
+
         node_indices[i] = i
-        ks[i] = len(node.connections)
+        ks[i] = k_i
+
+    ratio: float = max_k / min_k if min_k > 0 else 0.0
+    square_root_size: float = math.sqrt(len(list_nodes))
+    
+    print(f"size={len(list_nodes)}, ratio={ratio}, sqrt_size={square_root_size}")
+
+    print(f"max_k={max_k}, min_k={min_k}")
 
     plt.figure(figsize=(8, 6))
 
@@ -65,13 +90,13 @@ def prepare_probabilities(list_nodes: list[Node], kernel_size: int, step: int) -
     links_count: int = 0#2 * kernel_size * step
 
     for node in list_nodes:
-        links_count += len(node.connections)
+        links_count += len(node.neighbors)
 
     probabilities: list[float] = [0.0] * len(list_nodes)
 
     for i in range(len(list_nodes)):
         node: Node = list_nodes[i]
-        probabilities[i] = len(node.connections) / links_count
+        probabilities[i] = len(node.neighbors) / links_count
 
     return probabilities
 
@@ -86,9 +111,9 @@ def create_kernel(kernel_size: int) -> list[list[Node]]:
             node_i: Node = list_of_nodes[i]
             node_j: Node = list_of_nodes[j]
 
-            node_i.add_connection(node_j)
-            node_j.add_connection(node_i)
+            node_i.add_neighbor(node_j)
+            node_j.add_neighbor(node_i)
 
     return list_of_nodes
 
-main()
+run_ba_model(22220, 4)
